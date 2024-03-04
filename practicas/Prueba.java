b016e5312b5225bd4ceddbd5e918c80ee22d6254
RubenLegarda <159494601+RubenLegarda@users.noreply.github.com>
2024-03-04 16:58:00 -0600

    Add files via upload

---
+++ practicas/Prueba.java
@@ -0,0 +1,11 @@
+public class Prueba {
+
+import.java.util.Scanner;
+
+	public static void main(String args[]){
+		Scanner captura = new Scanner(System.in);
+		System.out.println("Introduce tu nombre: ");
+		String nombre = captura.nextLine();
+		System.out.println(nombre);
+	}
+}
